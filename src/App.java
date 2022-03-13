2f155f005c6bdba4802f213782692e3a8ce5fd3a
bane-rs <banelukety@gmail.com>
2022-03-13 23:17:28 +0100

    ovo je commit message

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Daj boze ako ikad ja ovo svarim :)");
     }
 }
